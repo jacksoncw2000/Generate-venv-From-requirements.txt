e7d3ff7f05a5fb43db9b45da1a9751c57f91bf10
Jackson Williams <78338284+jacksoncw2000@users.noreply.github.com>
2023-12-10 15:43:05 -0600

    Create create_venv.py

---
+++ create_venv.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import subprocess
+
+def create_venv(project_dir):
+    venv_dir = os.path.join(project_dir, 'myenv')
+
+    # Create virtual environment
+    subprocess.run([sys.executable, '-m', 'venv', venv_dir], check=True)
+
+    # Commands to activate venv and install requirements
+    activate_command = '. ' + os.path.join(venv_dir, 'bin', 'activate') if os.name != 'nt' else os.path.join(venv_dir, 'Scripts', 'activate')
+    install_command = f'pip install -r {os.path.join(project_dir, "requirements.txt")}'
+
+    return activate_command, install_command
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python create_venv.py <project_directory>")
+        sys.exit(1)
+
+    project_dir = sys.argv[1]
+    activate_command, install_command = create_venv(project_dir)
+
+    print("Virtual environment created.")
+    print("Run the following commands in your terminal:")
+    print(activate_command)
+    print(install_command)
